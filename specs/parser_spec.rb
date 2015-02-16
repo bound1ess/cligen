7981e281fc9b7ed91b946ea62066ebb21187faf2
bound1ess <bound1ess.dev@gmail.com>
2015-02-16 23:13:54 +0600

    Add Parser::get_sections.

--- specs/parser_spec.rb
+++ specs/parser_spec.rb
@@ -8,4 +8,15 @@ RSpec.describe(Cligen::Parser) do
     it("raises an exception if invalid file name was supplied") do
         expect { Cligen::Parser.new("invalid file name") }.to raise_error(RuntimeError)
     end
+
+    it("returns all described sections as a hash") do
+        sections = {
+            "Introduction" => "#introduction",
+            "Getting #started" => "#getting-started"
+        }
+
+        expect(@parser.get_sections).to eq(sections)
+        # Repeated on purpose.
+        expect(@parser.get_sections).to eq(sections)
+    end
 end
--- src/cligen/parser.rb
+++ src/cligen/parser.rb
@@ -7,6 +7,28 @@ module Cligen
             end
 
             @src = File.read(file)
+            @sections = nil
+        end
+
+        # Returns all sections (as a Hash) described in the file.
+        # An example of output: { "Doing s0m3th1ng @#$" => "#doing-s0m3th1ng" }
+        def get_sections
+            if not @sections.nil?
+                return @sections
+            end
+
+            sections = Hash.new
+
+            @src.scan(/^\/\/\s=section:(.+)$/).each do |match|
+                name, hashtag = match.first.clone, match.first
+
+                hashtag.gsub!(/\s/, "-")
+                hashtag.gsub!(/[^a-zA-Z0-9\-]+/, "")
+
+                sections[name.gsub(/\"+/, "")] = "#" + hashtag.downcase
+            end
+
+            @sections = sections
         end
     end
 end
