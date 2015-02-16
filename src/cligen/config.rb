bb24965c18e6b286078dc73ffecc3a2674663328
bound1ess <bound1ess.dev@gmail.com>
2015-02-16 13:44:17 +0600

    Write Cligen::Config.

--- src/cligen/config.rb
+++ src/cligen/config.rb
@@ -1,5 +1,21 @@
 module Cligen
+    require "yaml"
+
+    # This class manages Cligen configuration.
     class Config
-        # ...
+        # Attempts to load configuration from cligen.yml file.
+        # Receives a fully qualified path to the current working directory (as a string).
+        # Returns nil on failure, otherwise Hash.
+        def load(path)
+            if not path.include? "cligen.yml"
+                path = File.join(path, "cligen.yml")
+            end
+
+            if not File.exist?(path)
+                nil
+            end
+
+            YAML.load(File.read(path))
+        end
     end
 end
