25160535a69e66437a6c0ed6cf05b2264d0d399c
bound1ess <bound1ess.dev@gmail.com>
2015-02-17 20:03:35 +0600

    Add an executable + Main class.

---
+++ src/cligen/main.rb
@@ -0,0 +1,14 @@
+module Cligen
+    require_relative "config"
+    require_relative "parser"
+    require_relative "generator"
+    require_relative "cli"
+
+    # This class ties everything together and makes it work.
+    class Main
+        # Runs Cligen with given ARGV value.
+        def run(argv)
+            # ...
+        end
+    end
+end
