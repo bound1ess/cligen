aa04a8e0e02fe84309ebb3c3ca17fa7854cf22a1
bound1ess <bound1ess.dev@gmail.com>
2015-02-18 18:07:52 +0600

    Add generation time to templates.

--- src/cligen/main.rb
+++ src/cligen/main.rb
@@ -78,7 +78,7 @@ module Cligen
             }
 
             File.write(
-                File.join(Dir.getwd, "builds/main.html"),
+                File.join(Dir.getwd.include?("cligen") ? "builds" : Dir.getwd, "main.html"),
                 @generator.generate_page(@main_template, data)
             )
 
